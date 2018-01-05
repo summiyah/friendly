fbb699c9611ad3a270e535c9f803edee2dbac71b
Summiyah Siddeeq <summiyah@gmail.com>
2018-01-05 13:03:42 -0500

    project restarted after git issue

---
+++ app/data/friends.js
@@ -0,0 +1,71 @@
+var friendsArray = [
+    {
+        name: 'Mya',
+        photo: 'http://ssiddeeq.agnesscott.org/wp-content/uploads/2016/02/14523116_10101064590574462_3464486447615392620_n-240x300.jpg',
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1,
+        ]
+    },
+
+    {
+        name: 'Mya',
+        photo: 'http://ssiddeeq.agnesscott.org/wp-content/uploads/2016/02/14523116_10101064590574462_3464486447615392620_n-240x300.jpg',
+        scores: [
+            5,
+            1,
+            3,
+            5,
+            5,
+            3,
+            2,
+            4,
+            4,
+            1,
+        ]
+    },
+
+    {
+        name: 'Hanna',
+        photo: 'http://ssiddeeq.agnesscott.org/wp-content/uploads/2016/02/14523116_10101064590574462_3464486447615392620_n-240x300.jpg',
+        scores: [
+            5,
+            1,
+            3,
+            4,
+            1,
+            1,
+            2,
+            1,
+            2,
+            1,
+        ]
+    },
+
+    {
+        name: 'Lena',
+        photo: 'http://ssiddeeq.agnesscott.org/wp-content/uploads/2016/02/14523116_10101064590574462_3464486447615392620_n-240x300.jpg',
+        scores: [
+            5,
+            1,
+            2,
+            3,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+        ]
+    },
+];
+
+module.exports = friendsArray;
